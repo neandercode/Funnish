b20bd6a2f189f2869b7a9992b61ce689cc3dff37
neandercode <josephjones555@yahoo.com>
2024-03-25 21:08:56 -0400

    having fun with Objects 1-7

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,22 @@
+const car = {
+  type: "Ford",
+  model: "Fusion",
+  year: 2019,
+  color: "Silver",
+  trim: "Titanium",
+};
+
 function myMaff(p1, p2) {
   return p1 * p2;
 }
 
 let result = myMaff(42, 24);
-document.getElementById("demo").innerHTML = result;
+document.getElementById("demo").innerHTML =
+  "The car is a " +
+  car.year +
+  " " +
+  car.type +
+  " " +
+  car.model +
+  " " +
+  car.trim;
